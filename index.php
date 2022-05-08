8944ee6d1f6b621162ed047bd40f38a0e99dc206
Roosa Kontinen <roosa.kontinen@hotmail.com>
2022-05-08 22:26:47 +0300

    minor fixes

--- index.php
+++ index.php
@@ -20,7 +20,14 @@
             <div class="col">
                 <?php
                     $fields = "id,media_type,media_url,thumbnail_url,timestamp,permalink,caption";
-                    $accToken = ""; //Set your Access Token here.
+
+                    /*By default, Instagram API will give you a short-lived access token which is valid only for 24 hours.
+                    Instagram Longl-Lived Access Token is valid for 60 days.
+                    Long-lived tokens are valid for 60 days and can be refreshed as long as they are at least 24 hours old
+                    but have not expired, and the app user has granted your app the instagram_graph_user_profile permission. 
+                    Refreshed tokens are valid for 60 days from the date at which they are refreshed. */
+
+                    $accToken = "IGQVJXZA0tXRnoxb0tnbVNKVjZA3eUg1TjJvSE9mYktaRnNvaTc3akdkTExvU3dkekdIeXJmVFowV01VNlEzQm1uNDh2dFBqclZAFazFLdnRZAMS1JaTlpSDVFXzFkVUJMNjFiM3lzS3VfNnRibThwTmZAURAZDZD"; // Set your Access Token here.
                     $mediaLimit = 3; // Set a number of display items.
 
                     function gethData($url){
@@ -36,29 +43,27 @@
                     $result = gethData("https://graph.instagram.com/me/media?fields={$fields}&access_token={$accToken}&limit={$mediaLimit}");
                     $result_decode = json_decode($result, true);
 
-                    foreach ($result_decode["data"] as $post) : 
+                    foreach ($result_decode["data"] as $post) : {
 
-                    $caption = $post["caption"];
-                    $permalink = $post["permalink"];
-                    $media_type = $post["media_type"];
+                        $caption = $post["caption"];
+                        $permalink = $post["permalink"];
+                        $media_type = $post["media_type"];
 
-                    if ($media_type == "VIDEO" ) {
-                        $media_url = $post["thumbnail_url"]; }
-                    else {
-                        $media_url = $post["media_url"];
+                        if ($media_type == "VIDEO" ) {
+                            $media_url = $post["thumbnail_url"]; }
+                        else {
+                            $media_url = $post["media_url"];
+                        }
                     }
                     ?>
                     <div class="instagram_new">
                         <a class="insta-link" href="<?php echo $permalink; ?>" rel="noopener" target="_blank">
-                        <img src="<?php echo $media_url; ?>" loading="lazy" alt="<?php echo $caption; ?>" class="insta-image">
+                            <img src="<?php echo $media_url; ?>" loading="lazy" alt="<?php echo $caption; ?>" class="insta-image">
                         </a>
                     </div>
                 <?php endforeach; ?>
             </div>
         </div>
     </div>
-
-
-
 </body>
 </html>
