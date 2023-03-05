c79f2cf9e4e75111125f1c1bc42c1cec2ba8d1d2
Roosa Kontinen <70609940+Rykaee@users.noreply.github.com>
2023-03-06 00:18:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 /*
-Author: Roosa Kontinen 
+Author: Roosa Kontinen 2022
 */
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Insta</title>
+  <title>Instagram feed</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -15,7 +15,7 @@ Author: Roosa Kontinen
     <div class="container-fluid p-5 bg-dark text-white text-center">
         <div class="row">
             <div class="col">
-                <h1>Instagram syötteet</h1>
+                <h1>Instagram feed</h1>
             </div>
         </div>
     </div>
